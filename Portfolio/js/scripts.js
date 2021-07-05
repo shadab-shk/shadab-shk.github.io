6aff8f03fc2d4c7b35b9a7e5b77d42b4c5477df7
shadab-shk <shadab.shk2001@gmail.com>
2021-07-05 20:04:55 +0530

    Portfolio Added

---
+++ Portfolio/js/scripts.js
@@ -0,0 +1,66 @@
+/*!
+* Start Bootstrap - Resume v7.0.2 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// window.addEventListener('scroll',moveScrollindicator);
+
+// const scrollIndicatorElt = document.getElementById('scrollDiv');
+
+// const maxHeight = window.document.body.scrollHeight - window.innerHeight;
+
+// function moveScrollindicator(e){
+//     const percentage = ((window.scrollY) / maxHeight) * 100;
+
+//     scrollIndicatorElt.style.width = percentage + '%';
+// }
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("scrollDiv").style.width = scrolled + "%";
+}
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+          "body").style.visibility = "hidden";
+        document.querySelector(
+          "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+          "#loader").style.display = "none";
+        document.querySelector(
+          "body").style.visibility = "visible";
+    }
+};
